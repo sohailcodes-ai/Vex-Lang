e0a2fa8fc0849801853d811ca79499727efa7392
sohail <sohailali888822@gmail.com>
2026-06-19 23:38:20 +0530

    Add VM arithmetic and control flow tests

--- tests/test_vm.py
+++ tests/test_vm.py
@@ -29,6 +29,20 @@ class TestVexVM(unittest.TestCase):
 
         self.assertEqual(vm.globals["age"], 18)
 
+    def test_load_variable_and_print(self):
+        vm = VexVM()
+
+        instructions = [
+            Instruction(OpCode.LOAD_CONST, "Sohail"),
+            Instruction(OpCode.STORE_NAME, "naam"),
+            Instruction(OpCode.LOAD_NAME, "naam"),
+            Instruction(OpCode.PRINT),
+        ]
+
+        vm.run(instructions)
+
+        self.assertEqual(vm.output, ["Sohail"])
+
     def test_binary_addition(self):
         vm = VexVM()
 
@@ -42,6 +56,95 @@ class TestVexVM(unittest.TestCase):
         vm.run(instructions)
 
         self.assertEqual(vm.output, ["15"])
+
+    def test_arithmetic_precedence_bytecode_order(self):
+        vm = VexVM()
+
+        instructions = [
+            Instruction(OpCode.LOAD_CONST, 10),
+            Instruction(OpCode.LOAD_CONST, 5),
+            Instruction(OpCode.LOAD_CONST, 2),
+            Instruction(OpCode.BINARY_MUL),
+            Instruction(OpCode.BINARY_ADD),
+            Instruction(OpCode.PRINT),
+        ]
+
+        vm.run(instructions)
+
+        self.assertEqual(vm.output, ["20"])
+
+    def test_parenthesized_arithmetic_bytecode_order(self):
+        vm = VexVM()
+
+        instructions = [
+            Instruction(OpCode.LOAD_CONST, 10),
+            Instruction(OpCode.LOAD_CONST, 5),
+            Instruction(OpCode.BINARY_ADD),
+            Instruction(OpCode.LOAD_CONST, 2),
+            Instruction(OpCode.BINARY_MUL),
+            Instruction(OpCode.PRINT),
+        ]
+
+        vm.run(instructions)
+
+        self.assertEqual(vm.output, ["30"])
+
+    def test_compare_gte(self):
+        vm = VexVM()
+
+        instructions = [
+            Instruction(OpCode.LOAD_CONST, 18),
+            Instruction(OpCode.LOAD_CONST, 18),
+            Instruction(OpCode.COMPARE_GTE),
+        ]
+
+        vm.run(instructions)
+
+        self.assertEqual(vm.stack[-1], True)
+
+    def test_compare_lt(self):
+        vm = VexVM()
+
+        instructions = [
+            Instruction(OpCode.LOAD_CONST, 10),
+            Instruction(OpCode.LOAD_CONST, 18),
+            Instruction(OpCode.COMPARE_LT),
+        ]
+
+        vm.run(instructions)
+
+        self.assertEqual(vm.stack[-1], True)
+
+    def test_jump_if_false_skips_if_body(self):
+        vm = VexVM()
+
+        instructions = [
+            Instruction(OpCode.LOAD_CONST, False),
+            Instruction(OpCode.JUMP_IF_FALSE, 4),
+            Instruction(OpCode.LOAD_CONST, "wrong"),
+            Instruction(OpCode.PRINT),
+            Instruction(OpCode.LOAD_CONST, "correct"),
+            Instruction(OpCode.PRINT),
+        ]
+
+        vm.run(instructions)
+
+        self.assertEqual(vm.output, ["correct"])
+
+    def test_unconditional_jump(self):
+        vm = VexVM()
+
+        instructions = [
+            Instruction(OpCode.JUMP, 3),
+            Instruction(OpCode.LOAD_CONST, "wrong"),
+            Instruction(OpCode.PRINT),
+            Instruction(OpCode.LOAD_CONST, "correct"),
+            Instruction(OpCode.PRINT),
+        ]
+
+        vm.run(instructions)
+
+        self.assertEqual(vm.output, ["correct"])
 
 
 if __name__ == "__main__":
